f767cad46c35c6352589a273549741bea5840bfc
Chris Hodge <chrishodge801@gmail.com>
2021-12-13 13:14:39 -0700

    link sites

---
+++ catest/meetings.php
@@ -0,0 +1,328 @@
+
+ <div id="accordion">
+  <div class="card">
+    <div class="card-header" id="headingOne">
+      <h5 class="mb-0">
+        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+         Monday Meetings
+        </button>
+      </h5>
+    </div>
+
+    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+      <div class="card-body">
+        <div class="alert alert-primary" role="alert">
+                        A Vision For You<br>Noon<br>
+                        Fellowship Hall (Big Room)
+                        2060 South Windsor
+                        SLC, UT 84105<br><br>
+                        High Hopes<br> 6:30 p.m.<br>
+                        The Shack by the old Fairgrounds
+                        571 South 600 West
+                        Heber City, UT. 84032<br><br>
+                        Speedball<br> 7:30 p.m.<br>
+                        Fellowship Hall
+                        2060 South Windsor
+                        SLC, UT 84105<br><br>
+                        Crack Heads<br> 8 p.m.<br>
+                        496 W. State St.
+                        Orem, UT 84057<br><br>
+                        Monday Night Solution<br> 8 p.m.<br>
+                        2450 Fort Union Blvd.
+                        Cottonwood Heights, UT 84121<br><br>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingTwo">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Tuesday Meetings
+        </button>
+      </h5>
+    </div>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+      <div class="card-body">
+        <div class="alert alert-primary" role="alert">
+                            <b>Tuesday</b><br>
+                    A New Day A New Way<br>Noon<br>
+                    Fellowship Hall (Big Room)
+                    2060 South Windsor
+                    SLC, UT 84105<br><br>
+                    The Last Straw<br> 7 p.m.<br>
+                    12608 S. 125 W., Ste E
+                    Draper, UT 84020<br><br>
+                    Frosted Flakes<br> 8 p.m.<br>
+                    Rise and Shine Coffee
+                    7301 South 900 East #18
+                    Midvale, UT 84047<br><br>
+                    Pill Poppers and Pin Cushions (H&I)<br> 8 p.m.<br>
+                    341 N. 1100 E.
+                    American Fork, UT 84003<br><br>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingThree">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Wednesday Meetings
+        </button>
+      </h5>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+      <div class="card-body">
+        <div class="alert alert-primary" role="alert">
+                             <b>Wednesday</b><br>
+                      Home For Lunch <br>Noon<br>
+                      Fellowship Hall (Big Room)
+                      2060 South Windsor
+                      SLC, UT 84105<br><br>
+                      Carpet Farmers<br> 7 p.m.<br>
+                      Fellowship Hall
+                      2060 South Windsor
+                      SLC,UT 84105<br><br>
+                      Timp Rocks<br> 8 p.m.<br>
+                      Cirque Lodge
+                      777 N. Palisades Dr.
+                      Orem, Utah 84097<br><br>
+        </div>
+      </div>
+    </div>
+  </div>
+             <div class="card">
+    <div class="card-header" id="headingFour">
+      <h5 class="mb-0">
+        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+         Thursday Meetings
+        </button>
+      </h5>
+    </div>
+
+    <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+      <div class="card-body">
+        <div class="alert alert-primary" role="alert">
+                                <b>Thursday</b><br>
+                                Cocanoons <br>Noon<br>
+                                Fellowship Hall (Big Room)
+                                2060 South Windsor
+                                SLC, UT 84105<br><br>
+                                Time To Surrender<br> 7 p.m.<br>
+                                Ogden Alano Club (Big room upstairs)
+                                684 24th Street
+                                Ogden, UT 84401<br><br>
+                                Step Up<br> 7 p.m.<br>
+                                Roosevelt Alano Club
+                                144 E. 300 S.
+                                Roosevelt, UT 84066<br><br>
+                                Cocanots<br> 8:15 p.m.<br>
+                                Alano Club
+                                in the West Room
+                                5056 Commerce Dr. (S. 300 West)
+                                Murray, UT 84107<br><br>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingFive">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+          Friday Meetings
+        </button>
+      </h5>
+    </div>
+    <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
+      <div class="card-body">
+        <div class="alert alert-primary" role="alert">
+                         <b>Friday</b><br>
+                        Dark Side of the Spoon <br>Noon<br>
+                        Fellowship Hall (Big Room)
+                        2060 South Windsor
+                        SLC, UT 84105<br><br>
+                        Addicts R Us<br> 7 p.m.<br>
+                        South Davis Recovery Club
+                        Downstairs
+                        25 North 200 West
+                        Bountiful, UT 84010<br><br>
+                        Rock Hard<br> 7 p.m.<br>
+                        Central City Rec Center
+                        615 South 300 East
+                        SLC, UT 84111<br><br>
+                        Friday Night Few <br>7:30 p.m.<br>
+                        Ogden Alano Club
+                        (Big room upstairs)
+                        684 24th Street
+                        Ogden, UT 84401<br><br>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingSix">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+          Saturday Meetings
+        </button>
+      </h5>
+    </div>
+    <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordion">
+      <div class="card-body">
+        <div class="alert alert-primary" role="alert">
+                      <b>Saturday</b><br>
+                      Primary Purpose <br>Noon<br>
+                      Fellowship Hall (Small room)
+                      2060 South Windsor
+                      SLC, UT 84105<br><br>
+                      Saturday Night Live<br> 5 p.m.<br>
+                      Fellowship Hall
+                      2060 South Windsor
+                      SLC, UT 84105<br><br>
+                      Open H & I meeting<br> 7 p.m.<br>
+                      The Road Home
+                      210 S. Rio Grand St. (450 W.)
+                      Salt Lake City, UT 84101<br><br>
+                      Last Call<br> 10 p.m.<br>
+                      Rise and Shine Coffee
+                      7301 South 900 East #18
+                      Midvale UT, 84047<br><br>
+        </div>
+      </div>
+    </div>
+     <div class="card">
+    <div class="card-header" id="headingSeven">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+          Sunday Meetings
+        </button>
+      </h5>
+    </div>
+    <div id="collapseSeven" class="collapse" aria-labelledby="headingSeven" data-parent="#accordion">
+      <div class="card-body">
+        <div class="alert alert-primary" role="alert">
+                      <b>Sunday</b><br>
+                    O.R.E.O.S. <br>Noon<br>
+                    Fellowship Hall (Small room)
+                    2060 South Windsor
+                    SLC, UT 84105<br><br>
+                    Sunday Night Serenity
+                    Book Study <br>7 p.m.<br>
+                    Fellowship Hall (Small Room)
+                    2060 South Windsor
+                    SLC, UT 84105<br><br>
+                    Winners<br> 8 p.m.<br>
+                    U.N.I. (In Cafeteria)
+                    501 West Chipeta Way
+                    SLC, UT 84108<br><br>
+                    No Fronts 2.0<br> 6:30 p.m.<br>
+                    Building behind Church
+                    75 North 100 East
+                    American Fork, UT 84003<br><br>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+All meetings are open unless otherwise noted.
+
+    <hr class="featurette-divider">
+   </div><br>
+      <h2> <span class="text-muted">Upcoming Events</span></h2>
+      <div id="accordion">
+  <div class="card">
+    <div class="card-header" id="headingOne">
+      <h5 class="mb-0">
+        <button class="btn btn-link" data-toggle="collapse" data-target="#collapsePnrc" aria-expanded="false" aria-controls="collapsePnrc">
+         PNRC 2020
+        </button>
+      </h5>
+    </div>
+
+    <div id="collapsePnrc" class="collapse" aria-labelledby="headingPnrc" data-parent="#accordion">
+      <div class="card-body">
+                     <div class="alert alert-primary" role="alert">  
+        				<h2><b>PNRC 2020</b></h2><br>
+                    Some place<br>
+                    Some Date<br>
+                    Some Time<br>
+					Doing Something<br>
+          			Cost<br>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingCaws">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseCaws" aria-expanded="false" aria-controls="collapseCaws">
+          CAWS 2020
+        </button>
+      </h5>
+    </div>
+    <div id="collapseCaws" class="collapse" aria-labelledby="headingCaws" data-parent="#accordion">
+      <div class="card-body">
+        <div class="alert alert-primary" role="alert">
+          <h2><b>CAWS 2020</b></h2><br>
+                    Some place<br>
+                    Some Date<br>
+                    Some Time<br>
+					Doing Something<br>
+          			Cost<br>
+        </div>
+      </div>
+    </div>
+  </div>
+         <div class="collapse" id="collapseService">
+  <div class="card card-body">
+<p> 
+    </div>
+                    </div>
+
+         <button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#collapseService" aria-expanded="false" aria-controls="collapseService">
+   More on Service
+  </button>
+        In addition to finding a spiritual way of life, recovery is about changing negative aspects of our personalities into positive ones. We came into the program with big egos but little or no self-esteem. We thought we were better than other people yet, at the same time, felt “less than.” We were people who took from others and abused friendships all of our lives. We had no concept of doing anything for anyone without the thought of some kind of reward. By the sheer grace of our Higher Powers, we have found several ways of unlearning such behavior in the program. One way is to be of service to the Fellowship of Cocaine Anonymous. We discovered that the best way to serve God was to serve our fellow humans, and we found humility in the process. Our self-centered behavior was gradually replaced with the “attitude of service.” We learned that service is about gratitude and learning how to contribute to our lives and the lives of others.
+<br>
+Why Be of Service?<br>
+• To give back what was so freely given to us;<br>
+• To take on a commitment as a symbolic way of making amends;<br>
+• To meet other recovering addicts;<br>
+• To learn how to be part of a team;<br>
+• To learn humility by doing something selfless for someone else;<br>
+• To learn skills or teach others what we know;<br>
+• To learn responsibility.<br>
+<br>
+Ways to Be of Service<br>
+<br>
+<b>Service Opportunities at the Meeting and Area Level:</b><br>
+<b>Meeting Setup:</b> Arrive early to set up chairs, make coffee, and greet newcomers.<br>
+<b>Hotline:</b> Did you find C.A. through a hotline? Answer the phones for your Area’s hotline and be on the “front line” of helping others find C.A. and recovery.
+<b>Treasurer:</b> Pass the 7th Tradition basket and keep track of funds for a meeting—it’s a great way to learn responsibility.<br>
+<b>GSR/DSR (Group/District Service Representative):</b> Get involved in the business aspects of C.A. by representing one of your meetings or Districts at your District/Area business meeting.<br>
+<b>Secretary:</b> Ensure that a meeting will always be there for those who need it by choosing speakers, paying the rent, and keeping it running smoothly and consistently. You might even consider helping the Fellowship grow by starting a new meeting.<br>
+<br>
+<b>Committees at the Area and/or World Service Level:</b><br>
+<b>Convention:</b> Help plan big sober parties and carry the message of recovery at the same time.<br>
+<b>Literature:</b> If you have writing skills or ideas that could reach out in print to people who are still suffering, express those ideas on this World Service committee.<br>
+<b>Finance:</b> Put your accounting, business, or money management skills to good use in C.A.’s financial matters.<br>
+<b>Public Information:</b> Reach out through the media to those who have never heard of C.A.<br>
+<b>Hospitals & Institutions:</b> Help develop new ways to reach out to people who are unable to reach out for themselves.<br>
+<b>Unity:</b> Enhance communication and outreach among the diverse elements within our Fellowship.<br>
+<b>Structure & Bylaws:</b> Formulate bylaws, guidelines, and structures by which C.A. can operate.<br>
+<b>Conference:</b> Help organize the World Service Conference each year, where delegates from all over the world meet to conduct C.A. business.<br>
+<b>Area & World Service Office Boards of Directors:</b> Serve as a <b>Chairperson, Vice-Chairperson, Secretary, Treasurer, or Director at Large.</b> Conduct the day-to-day business operations of C.A. to ensure that it is there for newcomers the way it was there for you.<br>
+<br>
+Ask your GSR who you can contact for more information about how to be of service.<br>
+<br>
+Suggestions<br>
+• Share a commitment, such as a hotline slot, if your schedule doesn’t allow you to have one of your own.<br>
+• Find a place where you can be of service and then give 100%.<br>
+• Volunteer at a meeting when help is asked for or better yet, before it is asked for.<br>
+• Encourage the people you sponsor to take on commitments and then help them to keep those commitments.<br>
+• Ask your sponsor what type of service commitment you could take on that would best serve the Fellowship.<br>
+• Adopt the attitude of service without reward.<br>
+<br>
+          <i>Remember, time spent working on a commitment is time not spent getting loaded.</i></p>
+ 
